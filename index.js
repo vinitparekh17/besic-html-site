114c7f27cc5dd611ec43350109ae41f700fa4573
Vinit Parekh <77158947+vinitsoni17@users.noreply.github.com>
2021-11-11 14:25:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function change() {
+    var opt = document.getElementById("menu");
+
+    if(opt.value == "2") {
+        location.href = "./assets/drump/index.html"
+    }
+    if(opt.value == "1") {
+        location.href = "./assets/dicegame/index.html"
+    }
+    if(opt.value == "3") {
+        location.href = "./assets/digital clock/index.html"
+    }
+}
